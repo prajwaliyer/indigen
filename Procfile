b1e7ca63cc4692baac5b1a7c280b3baca40ad336
Prajwal Iyer <prajwaliyer@tamu.edu>
2024-02-09 15:43:47 -0600

    Static files

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: python backend/manage.py migrate
+release: python backend/manage.py migrate && python backend/manage.py collectstatic --noinput
 web: cd backend && gunicorn indigen_backend.wsgi:application --log-file -
--- backend/indigen_backend/settings.py
+++ backend/indigen_backend/settings.py
@@ -62,6 +62,7 @@ SITE_ID = 1
 MIDDLEWARE = [
     'social_django.middleware.SocialAuthExceptionMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
@@ -149,9 +150,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'build/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
